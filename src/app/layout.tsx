1e2c9251bdf8e7d236fc24fa4ea5ed440ba4dfc6
fluxteck <info.fluxteck@gmail.com>
2025-11-13 19:34:46 +0530

    Remove authentication components and update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,44 +3,36 @@ import "./globals.css";
 import Header from "@/components/Layout/Header";
 import Footer from "@/components/Layout/Footer";
 import { ThemeProvider } from "next-themes";
-import ScrollToTop from '@/components/ScrollToTop';
+import ScrollToTop from "@/components/ScrollToTop";
 import Aoscompo from "@/utils/aos";
 import { DonationProvider } from "./context/donationContext";
-import SessionProviderComp from "@/components/nextauth/SessionProvider";
-import { AuthDialogProvider } from "./context/AuthDialogContext";
 const montserrat = Montserrat({ subsets: ["latin"] });
-import NextTopLoader from 'nextjs-toploader';
+import NextTopLoader from "nextjs-toploader";
 
 export default function RootLayout({
   children,
-  session,
 }: Readonly<{
   children: React.ReactNode;
-  session:any
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={montserrat.className}>
-      <NextTopLoader color="#FF4D7E" />
-      <DonationProvider>
-        <AuthDialogProvider>
-      <SessionProviderComp session={session}>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={true}
-          defaultTheme="system"
-        >
-          <Aoscompo>
-            <Header />
-            
-            {children}
-            
-            <Footer />
-          </Aoscompo>
-          <ScrollToTop />
-        </ThemeProvider>
-        </SessionProviderComp>
-        </AuthDialogProvider>
+        <NextTopLoader color="#FF4D7E" />
+        <DonationProvider>
+          <ThemeProvider
+            attribute="class"
+            enableSystem={true}
+            defaultTheme="system"
+          >
+            <Aoscompo>
+              <Header />
+
+              {children}
+
+              <Footer />
+            </Aoscompo>
+            <ScrollToTop />
+          </ThemeProvider>
         </DonationProvider>
       </body>
     </html>
